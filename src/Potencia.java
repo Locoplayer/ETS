8cbeed9ff2fbdc5cc72d9e91ec8c81d674d486fd
Locoplayer <lucascasorial@gmail.com>
2025-04-23 10:13:24 +0100

    Programa Inicial

---
+++ src/Potencia.java
@@ -0,0 +1,58 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Potencia {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int res;
+        String mens;
+
+        mens ="Calculare la potencia de los números que introduzcas";
+        res=Potencia (teclado,mens);
+        mens ="El resultado de la operación es: "+res;
+        texto (mens);
+    }
+    public static int Potencia (Scanner teclado, String mensaje) {
+        int base;
+        int potencia;
+        int res=0;
+        String mens;
+
+        System.out.println(mensaje);
+        mens ="Introduce el número base (el que está elevado)";
+        base=leerEntero(teclado, mens);
+        mens ="Ahora la potencia";
+        potencia=leerEntero(teclado,mens);
+
+        if (potencia==0) {
+            res=1;
+        } else {
+            res=base;
+            potencia--;
+            int j;
+            for (j=potencia;j>0; j--) {
+                res=res*base;
+            }
+        }
+        return res;
+    }
+
+    public static int leerEntero(Scanner teclado, String mensaje) {
+        int num=0;
+
+        do {
+            try {
+                System.out.println(mensaje);
+                num= teclado.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Solo números");
+                teclado.next();
+            }
+        } while (num<0);
+        return num;
+    }
+
+    public static void texto (String mensaje) {
+        System.out.println(mensaje);
+    }
+}
